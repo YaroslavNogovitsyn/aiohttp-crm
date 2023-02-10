b758b286e4062894b7d85982482701bc44e38b41
Yaroslav <yaroslav.nogovitsyn@yandex.ru>
2023-02-10 20:27:37 +0300

    create model AddUserView and route /add_user

--- app/crm/routes.py
+++ app/crm/routes.py
@@ -1,5 +1,10 @@
-from app.web.app import Application
+import typing
+
+if typing.TYPE_CHECKING:
+    from app.web.app import Application
 
 
-def setup_routes(app: Application):
-    pass
+def setup_routes(app: "Application"):
+    from app.crm.views import AddUserView
+
+    app.router.add_view("/add_user", AddUserView)
--- app/web/routes.py
+++ app/web/routes.py
@@ -1,6 +1,9 @@
+import typing
+
 from app.crm.routes import setup_routes as crm_setup_routes
-from app.web.app import Application
+if typing.TYPE_CHECKING:
+    from app.web.app import Application
 
 
-def setup_routes(app: Application):
+def setup_routes(app: "Application"):
     crm_setup_routes(app)
