a91d015d159d6e11f1a712e7a13130e7896befab
Yaroslav <yaroslav.nogovitsyn@yandex.ru>
2023-02-10 20:51:39 +0300

    create CrmAccessor

--- app/web/app.py
+++ app/web/app.py
@@ -1,14 +1,19 @@
+from typing import Optional
+
 from aiohttp.web import \
     Application as AiohttpApplication, \
     Request as AiohttpRequest, \
     View as AiohttpView, \
     run_app as aiohttp_run_app
 
+from app.store import setup_accessor
+from app.store.crm.accessor import CrmAccessor
 from app.web.routes import setup_routes
 
 
 class Application(AiohttpApplication):
     database: dict = {}
+    crm_accessor: Optional[CrmAccessor] = None
 
 
 class Request(AiohttpRequest):
@@ -28,4 +33,5 @@ app = Application()
 
 def run_app():
     setup_routes(app)
+    setup_accessor(app)
     aiohttp_run_app(app, host="127.0.0.1")
