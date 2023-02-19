e336b2a7cbf995e63cd58c405965ec6af23492c4
Yaroslav <yaroslav.nogovitsyn@yandex.ru>
2023-02-19 11:42:17 +0300

    create middlewares

--- app/web/app.py
+++ app/web/app.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from aiohttp_apispec import setup_aiohttp_apispec
 
 from aiohttp.web import \
     Application as AiohttpApplication, \
@@ -8,6 +9,7 @@ from aiohttp.web import \
 
 from app.store import setup_accessor
 from app.store.crm.accessor import CrmAccessor
+from app.web.middlewares import setup_middlewares
 from app.web.routes import setup_routes
 
 
@@ -33,5 +35,7 @@ app = Application()
 
 def run_app():
     setup_routes(app)
+    setup_aiohttp_apispec(app, title='CRM Application', url='/docs/json', swagger_path='/docs')
+    setup_middlewares(app)
     setup_accessor(app)
     aiohttp_run_app(app, host="127.0.0.1")
