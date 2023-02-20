5efdcfa13729e74b95fec88efad8425ecb58a04a
Yaroslav <yaroslav.nogovitsyn@yandex.ru>
2023-02-20 18:28:08 +0300

    some changes

--- app/web/middlewares.py
+++ app/web/middlewares.py
@@ -16,14 +16,28 @@ async def error_handling_middleware(request, handler):
         response = await handler(request)
         return response
     except HTTPUnprocessableEntity as e:
-        return error_json_response(http_status=400, status='bad request', message=e.reason, data=json.loads(e.text))
+        return error_json_response(
+            http_status=400,
+            status='bad request',
+            message=e.reason,
+            data=json.loads(e.text)
+        )
     except HTTPException as e:
-        return error_json_response(http_status=e.status, status='error', message=str(e))
+        return error_json_response(
+            http_status=e.status,
+            status='error',
+            message=str(e)
+        )
     except Exception as e:
-        return error_json_response(http_status=500, status='internal server error', message=str(e))
+        return error_json_response(
+            http_status=500,
+            status='internal server error',
+            message=str(e)
+        )
 
 
 def setup_middlewares(app: "Application"):
     app.middlewares.append(error_handling_middleware)
     app.middlewares.append(validation_middleware)
+
 
